a6c56d9b3a841585a17e50f7a01dccd810a27350
JaacielBriseo <106225138+JaacielBriseo@users.noreply.github.com>
2022-06-06 21:46:43 -0700

    First commit

---
+++ javascript.js
@@ -0,0 +1,42 @@
+console.log('Externo');
+alert ('Bienvenido')
+
+var nombre = 'Jaaciel';
+var edad = 26;
+const fechaNacJaaciel = '23/02/1996'
+
+console.log(nombre);
+console.log(edad);
+console.log (nombre);
+
+console.log(
+    nombre + ' tiene ' + edad +  ' años' + ' porque nacio el ' + fechaNacJaaciel
+)
+
+// String (cadenas)
+var cadena = 'Cadena';
+var cadena3 = 'Cadenas';
+var cadena4;
+
+console.log(cadena)
+console.log(cadena3)
+console.log(cadena4)
+
+//Number
+var numero = 1;
+var Number = 0.01;
+var numero3 = -0.01;
+var numero4 = 1.5e+6;
+
+console.log(numero)
+console.log(Number)
+console.log(numero3)
+console.log(numero4)
+
+var infinito = 12341234532/0;
+var otro = 15 + + 'Cadena';
+
+console.log(infinito)
+console.log(otro)
+
+

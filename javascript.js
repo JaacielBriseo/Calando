1fe5edbe038aa33ce6505f10bf428d34439df500
JaacielBriseo <106225138+JaacielBriseo@users.noreply.github.com>
2022-06-15 21:24:45 -0700

    valores y operadores

--- javascript.js
+++ javascript.js
@@ -66,3 +66,75 @@ if (quincena >1000 ) {
     else {
         console.log ('Te fue mal')
     }
+
+var puntaje = 150;
+/**
+ *  puntaje = 10 => Logro I
+ *  puntaje = 50 => Logro II
+ * puntaje = 150 => Logro III
+ */
+if (puntaje < 10){
+    console.log ('Echale más ganas !!');
+} if (puntaje >= 10 ) {
+    console.log ('Ganaste el logro I!!!');
+} if (puntaje >= 50 ) {
+    console.log ('Ganaste el logro II!!!');
+} if (puntaje >= 150 ) {
+    console.log ('Ganaste el logro III!!!');
+}
+// && => OPERADOR AND (todos deben estar correctos)
+if (puntaje >= 10 && puntaje >= 50 && puntaje >= 150){
+    console.log ('Felicidades , ganaste los 3 logros!!')
+}
+// || => OPERADOR OR (almenos uno debe estar correcto) 24:24
+if (puntaje >= 10 || puntaje >= 50 || puntaje >= 150){
+    console.log ('Tienes un logro, sigue asi!!!')
+}
+// Booleans (Letra Z es de mayor valor que A)
+console.log ( 10<20)
+console.log ( 10>20)
+console.log ('A' > 'Z')
+console.log ('A' < 'Z')
+
+//Variables (Cualquier forma sirve)
+let Arturo = 'El mejor';
+console.log (Arturo)
+var Isa = 'La mejor'
+console.log (Isa)
+const ale = 500
+console.log (ale)
+
+if (Isa < Arturo) {console.log ( 'eaea')
+    
+} else { console.log ('asdfasfdg')
+    
+}
+//While (Me imprime los numeros del 1 al 10 )
+let x= 1;
+while ( x<11){
+    console.log (x)
+    x = x + 1;
+}
+let w = 2 ;
+while ( w < 12){
+    console.log (w)
+    y = w++
+}
+// do y prompt
+/*let tuNombre = 'Jaaciel';
+do {prompt ('Quien eres?');
+
+}while (tuNombre = Jaaciel );
+console.log (tuNombre)*/
+
+
+//For
+for (let number = '#'; number <= '########'; number = number + '#') {
+    console.log(number);
+  }
+
+let gato = ''
+while ( gato != '########') {
+    gato = gato + '#'
+    console.log ( gato )
+}
